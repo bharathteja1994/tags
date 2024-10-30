17da4d7019028ab9f881f5bed7bb4f1f81ace8a3
bharathteja1994 <77113760+bharathteja1994@users.noreply.github.com>
2024-10-30 08:45:56 -0400

    Create main.tf

---
+++ examples/main.tf
@@ -0,0 +1,31 @@
+module "mandatory_tags" {
+  source               = "./tags"
+  version              = "0.0.2"
+
+  tags                 = {}
+
+  apm-id                   = var.apm-id
+  application-name         = var.application-name
+  app-support-dl           = var.app-support-dl
+  app-servicenow-group     = var.app-servicenow-group
+  business-division        = var.business-division
+  compliance               = var.compliance
+  company                  = var.company
+  costcenter               = var.costcenter
+  environment              = var.environment
+  PatchGroup               = var.PatchGroup
+  PatchWindow              = var.PatchWindow
+  workspace                = var.ATLAS_WORKSPACE_NAME
+  
+  elvh-apm-id               = var.elvh-apm-id
+  elvh-app-env              = var.elvh-app-env
+  elvh-application-name     = var.elvh-application-name
+  elvh-app-support-dl       = var.elvh-app-support-dl
+  elvh-app-servicenow-group = var.elvh-app-servicenow-group
+  elvh-business-division    = var.elvh-business-division
+  elvh-ci-owner             = var.elvh-ci-owner
+  elvh-company              = var.elvh-company
+  elvh-costcenter           = var.elvh-costcenter
+  elvh-infra-env            = var.elvh-infra-env  
+  elvh-workspace            = var.ATLAS_WORKSPACE_NAME
+}
